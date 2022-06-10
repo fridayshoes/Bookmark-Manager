a50adeaa5bdd32a908da2e8e22a9e773d11a10d6
fridayshoes <one_amp@hotmail.com>
2022-06-10 18:02:41 +0100

    Wrapping Database Data in Program Objects

--- spec/features/add_bookmarks_spec.rb
+++ spec/features/add_bookmarks_spec.rb
@@ -1,8 +1,10 @@
-feature 'testing add bookmark' do
-  scenario 'user adds bookmark via a form' do
+feature 'Adding a new bookmark' do
+  scenario 'A user can add a bookmark to Bookmark Manager' do
     visit('/bookmarks/new')
-    fill_in :url, with: 'http://mashable.com'
+    fill_in('url', with: 'http://www.example.org')
+    fill_in('title', with: 'Test Bookmark')
     click_button('Submit')
-    expect(page).to have_content 'http://mashable.com'
+
+    expect(page).to have_link('Test Bookmark', href: 'http://www.example.org')
   end
 end
